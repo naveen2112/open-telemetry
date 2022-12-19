9f4e4ba44b2ccd93ec6173962064e63edfd2c0b3
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-19 23:41:50 +0530

    Separated the table name from column definitions

--- hubble_reports/models/project_users.py
+++ hubble_reports/models/project_users.py
@@ -2,6 +2,7 @@ from .core import db
 
 class ProjectUsers(db.Model):
     __tablename__ = 'project_users'
+    
     user_id = db.Column('user_id', db.ForeignKey('users.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False, primary_key=True)
     project_id = db.Column('project_id', db.ForeignKey('projects.id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False)
     role_type = db.Column('role_type', db.String(255))
