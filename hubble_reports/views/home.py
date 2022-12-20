949cd8559e25645d656a888131aafe4fd6927574
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-20 19:27:31 +0530

    updated to check the SSO login

--- hubble_reports/views/home.py
+++ hubble_reports/views/home.py
@@ -1,6 +1,14 @@
+from flask import session, url_for, render_template, redirect
+import msal
 from hubble_reports.hubble_reports import reports
 
 
+# @reports.route("/")
+# def hello_world():
+#     return "<p>Hello World, Welcome to Home Page!</p>"
+
 @reports.route("/")
-def hello_world():
-    return "<p>Hello World, Welcome to Home Page!</p>"
+def index():
+    if not session.get("user"):
+        return redirect(url_for("login"))
+    return render_template('index.html', user=session["user"], version=msal.__version__)
