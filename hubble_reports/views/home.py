f3bdf0829d732587a9e10b623abcf3fe727814df
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-21 15:16:29 +0530

    Removed unnessary details

--- hubble_reports/views/home.py
+++ hubble_reports/views/home.py
@@ -1,5 +1,5 @@
 from flask import session, url_for, render_template, redirect
-import msal
+
 from hubble_reports.hubble_reports import reports
 
 
@@ -10,5 +10,5 @@ from hubble_reports.hubble_reports import reports
 @reports.route("/")
 def index():
     if not session.get("user"):
-        return redirect(url_for("login"))
-    return render_template('index.html', user=session["user"], version=msal.__version__)
+        return redirect(url_for("reports.login"))
+    return render_template('index.html', user=session["user"])
