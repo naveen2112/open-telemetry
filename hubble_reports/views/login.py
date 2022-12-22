b0abec554dbb012599ad852ced5875c29544f953
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-22 12:01:16 +0530

    Implemented logging in flask for verification

--- hubble_reports/views/login.py
+++ hubble_reports/views/login.py
@@ -3,18 +3,19 @@ import msal
 from hubble_reports.hubble_reports import reports
 from config import BaseConfig
 import logging
-from hubble_reports import templates
+from hubble_reports.utils import get_logger
 
+logger = get_logger(__name__,level=logging.DEBUG)
 
 
 @reports.route("/login")
 def login():
-    print("\n\n\n*******\n\n\n")
+    logger.info(f"\n\n========Login=======\n")
     # Technically we could use empty list [] as scopes to do just sign in,
     # here we choose to also collect end user consent upfront
-    session["flow"] = _build_auth_code_flow(authority=BaseConfig.AUTHORITY_SIGN_ON_SIGN_OUT)
-    print('\n\n\n',session["flow"],'\n\n')
-    return render_template("login.html", auth_url=session["flow"])#["auth_uri"])
+    session["flow"] = _build_auth_code_flow(authority=BaseConfig.AUTHORITY_SIGN_ON_SIGN_OUT)   
+    logger.debug(f"\n=============>>>Session\n{session['flow']}\n")
+    return render_template("login.html", auth_url=session["flow"]["auth_uri"])
 
 @reports.route(BaseConfig.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
 def authorized():
@@ -22,8 +23,10 @@ def authorized():
         cache = _load_cache()
         result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
             session.get("flow", {}), request.args)
+        logger.debug(f"\n=============>>>Result\n{result}\n")
         if "error" in result:
             return render_template("auth_error.html", result=result)
+
         session["user"] = result.get("id_token_claims")
         _save_cache(cache)
     except ValueError:  # Usually caused by CSRF
@@ -35,11 +38,14 @@ def _load_cache():
     cache = msal.SerializableTokenCache()
     if session.get("token_cache"):
         cache.deserialize(session["token_cache"])
+    logger.debug(f"\n=============>>>Cache Deserialized\n{cache}\n")
     return cache
 
 def _save_cache(cache):
     if cache.has_state_changed:
         session["token_cache"] = cache.serialize()
+        logger.debug(f"\n=============>>>Token_cache\n{session}\n")
+             
 
 def _build_auth_code_flow(authority=None, scopes=None):
     return _build_msal_app(authority=authority).initiate_auth_code_flow(
