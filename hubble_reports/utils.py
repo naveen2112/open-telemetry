63f317567289c8073cf379415eac64bf7caa0f02
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-26 16:33:41 +0530

    Formatted the utils.py for space and type hints

--- hubble_reports/utils.py
+++ hubble_reports/utils.py
@@ -1,35 +1,25 @@
 import logging
 
+from typing import Union
 from functools import wraps
 from flask import Blueprint, session, redirect
 
 def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
 
     logger = logging.getLogger(name)
-
     logger.setLevel(level)
-
-
-
     console = logging.StreamHandler()
-
     formatter = logging.Formatter("%(asctime)s [%(levelname)s %(name)s] %(message)s")
-
     console.setFormatter(formatter)
-
     logger.addHandler(console)
-
-
 
     return logger
 
 
-def login_required(func):
-    """
-    Login required for all routes
-    """
+def login_required(func) -> function:
+ 
     @wraps(func)
-    def inner(*args, **kwargs):
+    def inner(*args, **kwargs) -> Union(function, redirect):
         if session.get('user'):
             return func(*args, **kwargs)
         else:
