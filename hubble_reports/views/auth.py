0f0c6c0aedf83490d2addb3bd055627a79e40416
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-26 17:38:59 +0530

    Created new two routes to test 403 and 500 error

--- hubble_reports/views/auth.py
+++ hubble_reports/views/auth.py
@@ -3,6 +3,7 @@ from hubble_reports.hubble_reports import reports
 from hubble_reports.models import User
 import logging
 from hubble_reports.utils import get_logger, login_required
+from flask import abort
 
 logger = get_logger(__name__,level=logging.DEBUG)
 
@@ -12,3 +13,12 @@ def auth() -> str:
     logger.info(f"\n\n\n\n========Auth=======\n")
     user = db.get_or_404(User, 64)
     return str(user.email)
+
+@reports.route("/protected", methods=['POST'])
+def protected() -> str:
+    page_info = 'Protected access'
+    return page_info
+
+@reports.route("/server-error")
+def server_error() -> str:
+    abort(500)
