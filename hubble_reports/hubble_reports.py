12d043bf4112f08d54f4adc2177ed78efca0f3f6
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-28 13:14:13 +0530

    Update to check role before querying permission

--- hubble_reports/hubble_reports.py
+++ hubble_reports/hubble_reports.py
@@ -1,26 +1,37 @@
 from itertools import chain
-from flask import Blueprint, session, g
+from flask import Blueprint, session, g, abort
 from hubble_reports.models import db, User, RoleUser, PermissionRole, Permission
 import logging
 from hubble_reports.utils import get_logger
 
-reports = Blueprint('reports', __name__, template_folder='templates')
+reports = Blueprint("reports", __name__, template_folder="templates")
 
 from hubble_reports import views
 
-global user_perm 
-logger = get_logger(__name__,level=logging.DEBUG)
+global user_perm
+logger = get_logger(__name__, level=logging.DEBUG)
+
+
 @reports.before_request
 def user_permission():
-    if 'user' in session.keys():
-        mailid = session['user']['preferred_username']
-        user_perm = db.session.query(Permission.name)\
-                        .join(PermissionRole, PermissionRole.permission_id == Permission.id)\
-                            .join(RoleUser, RoleUser.role_id == PermissionRole.role_id)\
-                                .join(User, User.id == RoleUser.user_id)\
-                                    .filter(User.email==mailid)
-        g.user_perm = set(chain.from_iterable(user_perm))
-
+    if "user" in session.keys():
+        mailid = session["user"]["preferred_username"]
+        user_role_ids = (
+            db.session.query(RoleUser.role_id)
+            .join(User, User.id == RoleUser.user_id)
+            .filter(User.email == mailid)
+        )
+        if user_role_ids is not None:
+            user_perm = (
+                db.session.query(Permission.name)
+                .join(PermissionRole, PermissionRole.permission_id == Permission.id)
+                .filter(PermissionRole.role_id.in_(*user_role_ids))
+                .all()
+            )
+            logger.info(f"\n\n\n\n\n============>User permission in before request:\n\n{user_perm}\n\n")
+            g.user_perm = set(chain.from_iterable(user_perm))
+        else:
+            abort(403)
     else:
         ...
 
