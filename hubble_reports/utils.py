f66dd4a121f96f6810753e7a7724d32fcd2a8382
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-28 12:01:03 +0530

    verify_permission check decorator is created

--- hubble_reports/utils.py
+++ hubble_reports/utils.py
@@ -1,4 +1,6 @@
 import logging
+from functools import wraps
+from flask import abort, session, g
 
 def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
 
@@ -10,3 +12,20 @@ def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
     logger.addHandler(console)
 
     return logger
+
+loggered = get_logger(__name__,level=logging.DEBUG)
+
+def verify_permission(*permissions_allowed: tuple[str]):
+    def outer(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            permissions_set = set(permissions_allowed)
+            loggered.info(f"\n\n\n==========>>>Verify Permission:\n\n")
+            loggered.info(f"\n\n\n==========>>>Permission allowed:\n{permissions_set}\n\n")
+            loggered.info(f"\n\n\n==========>>>User Permissions:\n{g.user_perm}\n\n")
+            if g.user_perm is not None and not permissions_set.isdisjoint(g.user_perm):
+                return f(*args, **kwargs)
+            else:
+                abort(403)
+        return inner
+    return outer
