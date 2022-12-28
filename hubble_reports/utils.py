f10a0350c01d0b2f705c00bbcc9aa8d1446686b4
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-28 18:15:41 +0530

    Removed the spaces in logger function

--- hubble_reports/utils.py
+++ hubble_reports/utils.py
@@ -1,21 +1,10 @@
 import logging
 
 def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
-
     logger = logging.getLogger(name)
-
     logger.setLevel(level)
-
-
-
     console = logging.StreamHandler()
-
     formatter = logging.Formatter("%(asctime)s [%(levelname)s %(name)s] %(message)s")
-
     console.setFormatter(formatter)
-
     logger.addHandler(console)
-
-
-
     return logger
