3090369bf53203ee5b5b15b784b9be0aea82c28b
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-28 12:00:00 +0530

    Update before & after request in the reports

--- hubble_reports/hubble_reports.py
+++ hubble_reports/hubble_reports.py
@@ -1,5 +1,31 @@
-from flask import Blueprint
+from itertools import chain
+from flask import Blueprint, session, g
+from hubble_reports.models import db, User, RoleUser, PermissionRole, Permission
+import logging
+from hubble_reports.utils import get_logger
 
 reports = Blueprint('reports', __name__, template_folder='templates')
 
 from hubble_reports import views
+
+global user_perm 
+logger = get_logger(__name__,level=logging.DEBUG)
+@reports.before_request
+def user_permission():
+    if 'user' in session.keys():
+        mailid = session['user']['preferred_username']
+        user_perm = db.session.query(Permission.name)\
+                        .join(PermissionRole, PermissionRole.permission_id == Permission.id)\
+                            .join(RoleUser, RoleUser.role_id == PermissionRole.role_id)\
+                                .join(User, User.id == RoleUser.user_id)\
+                                    .filter(User.email==mailid)
+        g.user_perm = set(chain.from_iterable(user_perm))
+
+    else:
+        ...
+
+
+@reports.after_request
+def user_permission(response):
+    g.user_perm = None
+    return response
