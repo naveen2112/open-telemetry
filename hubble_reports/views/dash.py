045ce93505d535b8c54b7f18960625f7d60b3b54
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-30 13:24:37 +0530

    Update dash to show table remove csrf expression

--- hubble_reports/views/dash.py
+++ hubble_reports/views/dash.py
@@ -4,10 +4,12 @@ import numpy as np
 
 import pathlib
 from hubble_reports.hubble_reports import reports
-from flask import render_template_string, current_app, url_for
+from flask import render_template_string, current_app, url_for, redirect, session
+from flask_login import login_required, logout_user
 from sqlalchemy import create_engine
 from flask.helpers import get_root_path
-from dash import Dash
+from dash.dependencies import Input, Output
+from dash import Dash, dash_table
 from dash import Dash, dcc, html
 from config import BaseConfig
 from app import app
@@ -18,13 +20,13 @@ import logging
 
 logger = get_logger(__name__,level=logging.DEBUG)
 
-app_dash = Dash(
+dash_app = Dash(
     __name__,
     server=app,
     url_base_pathname='/dash/',
 )
 
-
+# session['dash'].header = f"Welcome, {session['user'].name}!!!"
 # FYI, you need both an app context and a request context to use url_for() in the Jinja2 templates
 with app.app_context(), app.test_request_context():
     layout_dash = pathlib.Path(get_root_path(__name__)).parent.joinpath("templates").joinpath("dashboard.html")
@@ -32,16 +34,19 @@ with app.app_context(), app.test_request_context():
     # logger.info(f"\n\n\n\n=========>>>template url_for:\n{url_for('templates', filename='layouts/base.html')}\n\n")
     with open(layout_dash, "r") as f:
         html_body = render_template_string(f.read())
+        html_body = html_body.replace('HEADER', 'Welcome User!!!')
         print('\n\n\n', html_body, '\n\n\n')
         
-        app_dash.index_string = (html_body)
+        dash_app.index_string = (html_body)
 
     db_conn = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
 
     df_or = pd.read_sql_query(db.session.query(
         TimesheetEntry.authorized_hours, 
+        TimesheetEntry.entry_date,
         Team.name, 
-        ExpectedUserEfficiency.expected_efficiency)
+        ExpectedUserEfficiency.expected_efficiency,
+        )
         .join(TimesheetEntry, 
             TimesheetEntry.user_id == ExpectedUserEfficiency.user_id)
             .join(Team, Team.id == TimesheetEntry.team_id).statement, 
@@ -49,23 +54,56 @@ with app.app_context(), app.test_request_context():
 
     df = df_or.copy()
 
+    df.rename(columns={
+        'authorized_hours':'actual_efficiency', 
+        'name':'team',
+        'entry_date':'date',
+        }, 
+        inplace=True
+    )
 
-    df.loc[:, ('percentage')] = 0
-    df.loc[:, 'percentage'] = 100 * df['authorized_hours']/df['expected_efficiency']
-    df['percent_processed'] = 0
-    df['percent_processed'] = df['percentage'].fillna(0)
+    df.loc[:, 'capacity'] = 100 * df['actual_efficiency']/df['expected_efficiency']
+    df['capacity'] = df['capacity'].fillna(0)
     df.replace(np.inf, 100.0, inplace=True)
+    # data['date']=pd.to_datetime(data['date'],format='%Y-%m-%d')
+    df['date'] = pd.to_datetime(df['date'], format=r"%Y-%m-%d")
     logger.info(f"\n\n\n\n========Info=======\n{df}\n")
-    df = df.groupby('name').mean(numeric_only=True)['percent_processed'].reset_index()
-    fig_q1 = px.bar(df, x='name', y='percent_processed', color='name', text='percent_processed').update_traces(texttemplate='%{text:.2f}')
-    app_dash.layout = html.Div(children=[
-        html.H2(children='Productivity vs Efficiency for Team'),
+    min_year= df['date'].min().year
+    max_year= df['date'].max().year
+
+    till_date= df['date'].max().strftime("%B %Y")
+    df = df.groupby('team').mean(numeric_only=True)['capacity'].reset_index()
+    fig_q1 = px.bar(df, x='team', y='capacity', color='team', text='capacity').update_traces(texttemplate='%{text:0.0f}%')
+    df['ratings'] = ['Excellent' if val>121 else 'Good' if val>120 else 'Needs Improvement' for val in df['capacity']]
+    df['trends'] = df['capacity'].apply(lambda a: "↑" if a>121 else "↔︎" if a>120 else "↓")
+    # min_year, max_year, till_date = (df['date'].min().year, df['date'].max().year, df['date'].format("%B %Y"))
+    
+    dash_app.layout = html.Div(children=[
+        html.H1(id="dash_header"),
+        html.H2(children=f'Teams Efficiency bandwidth- Fiscal Year {min_year} - {max_year} (Till, {till_date})'),
         dcc.Graph(
-            figure=fig_q1
+            figure=fig_q1,
+            id="overall_efficiency"
         ),
+        dash_table.DataTable(
+            data=df.to_dict('records'), 
+            columns=[{"name": i, "id": i} for i in df.columns],
+            style_cell={'textAlign': 'left'},
+            # style_header={'textBold':True},
+            )
+
     ])
 
+@dash_app.callback(
+    Output("dash_header", 'children'),
+    Input("overall_efficiency", 'clickData'),
+    )
+def dash_into():
+    # session['dash'].header = f"Welcome, {session['user'].name}!!!"
+    return f"Welcome, {session['user'].name}!!!"
 
-# @reports.route("/dash")
-# def dash_app():
-#     return app_dash.index()
+
+@reports.route("/dash")
+# @login_required
+def dash_app():
+    return dash_app.index()
