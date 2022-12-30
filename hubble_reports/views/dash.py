c57770ca6407adb48c8a71e2f5f0d2eb7a6e6746
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2022-12-30 18:36:34 +0530

    Update graph label, link report url in sidebar

--- hubble_reports/views/dash.py
+++ hubble_reports/views/dash.py
@@ -34,6 +34,7 @@ dash_app = Dash(
 # dash.register_page("home", layout=html.Div('home'), path='/')
 # session['dash'].header = f"Welcome, {session['user'].name}!!!"
 # FYI, you need both an app context and a request context to use url_for() in the Jinja2 templates
+# def dash_call():
 with app.app_context(), app.test_request_context():
     layout_dash = pathlib.Path(get_root_path(__name__)).parent.joinpath("templates").joinpath("dashboard.html")
     logger.info(f"\n\n\n\n=========>>>layout_dash:\n{layout_dash}\n\n")
@@ -43,87 +44,104 @@ with app.app_context(), app.test_request_context():
         html_body = html_body.replace('HEADER', 'Welcome User!!!')
         print('\n\n\n', html_body, '\n\n\n')
         
-        dash_app.index_string = (html_body)
+dash_app.index_string = (html_body)
 
-    db_conn = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
+db_conn = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
 
-    df_or = pd.read_sql_query(db.session.query(
-        TimesheetEntry.authorized_hours, 
-        TimesheetEntry.entry_date,
-        Team.name, 
-        ExpectedUserEfficiency.expected_efficiency,
-        )
-        .join(TimesheetEntry, 
-            TimesheetEntry.user_id == ExpectedUserEfficiency.user_id)
-            .join(Team, Team.id == TimesheetEntry.team_id).statement, 
-            db_conn)
-
-    df = df_or.copy()
-
-    df.rename(columns={
-        'authorized_hours':'actual_efficiency', 
-        'name':'team',
-        'entry_date':'date',
-        }, 
-        inplace=True
+df_or = pd.read_sql_query(db.session.query(
+    TimesheetEntry.authorized_hours, 
+    TimesheetEntry.entry_date,
+    Team.name, 
+    ExpectedUserEfficiency.expected_efficiency,
     )
+    .join(TimesheetEntry, 
+        TimesheetEntry.user_id == ExpectedUserEfficiency.user_id)
+        .join(Team, Team.id == TimesheetEntry.team_id).statement, 
+        db_conn)
 
-    df.loc[:, 'capacity'] = 100 * df['actual_efficiency']/df['expected_efficiency']
-    df['capacity'] = df['capacity'].fillna(0)
-    df.replace(np.inf, 100.0, inplace=True)
-    # data['date']=pd.to_datetime(data['date'],format='%Y-%m-%d')
-    df['date'] = pd.to_datetime(df['date'], format=r"%Y-%m-%d")
-    logger.info(f"\n\n\n\n========Info=======\n{df}\n")
-    min_year= df['date'].min().year
-    max_year= df['date'].max().year
+df = df_or.copy()
 
-    till_date= df['date'].max().strftime("%B %Y")
-    df = df.groupby('team').mean(numeric_only=True)['capacity'].reset_index()
-    fig_q1 = px.bar(df, x='team', y='capacity', color='team', text='capacity').update_traces(texttemplate='%{text:0.0f}%')
-    df['ratings'] = ['Excellent' if val>121 else 'Good' if val>120 else 'Needs Improvement' for val in df['capacity']]
-    df['trends'] = df['capacity'].apply(lambda a: "↑" if a>121 else "↔︎" if a>120 else "↓")
-    # min_year, max_year, till_date = (df['date'].min().year, df['date'].max().year, df['date'].format("%B %Y"))
-    df['capacity'] = df['capacity']/100
-    
-    dash_app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-    ])
+df.rename(columns={
+    'authorized_hours':'actual_efficiency', 
+    'name':'team',
+    'entry_date':'date',
+    }, 
+    inplace=True
+)
+
+df.loc[:, 'capacity'] = 100 * df['actual_efficiency']/df['expected_efficiency']
+df['capacity'] = df['capacity'].fillna(0)
+df.replace(np.inf, 100.0, inplace=True)
+# data['date']=pd.to_datetime(data['date'],format='%Y-%m-%d')
+df['date'] = pd.to_datetime(df['date'], format=r"%Y-%m-%d")
+logger.info(f"\n\n\n\n========Info=======\n{df}\n")
+min_year= df['date'].min().year
+max_year= df['date'].max().year
+
+till_date= df['date'].max().strftime("%B %Y")
+df = df.groupby('team').mean(numeric_only=True)['capacity'].reset_index()
+fig_q1 = (px.bar(df, x='team', y='capacity', 
+color='team', text='capacity', 
+title="Teams Capacity - Efficiency %",
+labels={'team':'Teams', 'capacity': 'Capacity'}
+)
+.update_traces(texttemplate='%{text:0.0f}%').update_layout(title_x=0.5))
+df['ratings'] = ['Excellent' if val>121 else 'Good' if val>120 else 'Needs Improvement' for val in df['capacity']]
+df['trends'] = df['capacity'].apply(lambda a: "↑" if a>121 else "↔︎" if a>120 else "↓")
+# min_year, max_year, till_date = (df['date'].min().year, df['date'].max().year, df['date'].format("%B %Y"))
+df['capacity'] = df['capacity']/100
+
+dash_app.layout = html.Div([
+dcc.Location(id='url', refresh=False),
+html.Div(id='page-content')
+])
 
 
-    index_page = html.Div([
-        dcc.Link('Teams Capacity', href='/dash/capacity'),
-        # html.Br(),
-        # dcc.Link('Go to Page 2', href='/page-2'),
-    ])
+index_page = html.Div([
+    html.H1(children=f"g.user")
+    # html.Br(),
+    # dcc.Link('Go to Page 2', href='/page-2'),
+])
 
-    layout = html.Div(id='overall_eff',children=[
-        html.H1(id="dash_header",children=f'Overall Team wise Efficiency Percentage', style={"font-size":"25px", "font-align":"center"}),
-        html.Br(),
-        html.H2(children=f'Teams Efficiency bandwidth- Fiscal Year {min_year} - {max_year} (Till, {till_date})'),
-        dcc.Graph(
-            figure=fig_q1,
-            id="overall_efficiency"
-        ),
-        dash_table.DataTable(
-            data=df.to_dict('records'), 
-            # columns=[{"name": i, "id": i} for i in df.columns],
-            columns=[
-                {'name': ['Teams Ratings & Trend','Team'], 'id': 'team', 'type': 'text'},
-                {'name': ['Teams Ratings & Trend','Capacity'], 'id': 'capacity', 'type': 'numeric', 'format':dash_table.FormatTemplate.percentage(0)},
-                {'name': ['Teams Ratings & Trend','Ratings'], 'id': 'ratings', 'type': 'text'},
-                {'name': ['Teams Ratings & Trend','Trends'], 'id': 'trends', 'type': 'text'},
-            ],
-            merge_duplicate_headers=True,
-            style_cell={'textAlign': 'left', 'fontSize':'20px'},
-            style_header={
-                'backgroundColor': 'orange',
-                'fontWeight': 'bold',
-                'textAlign':'center',
-            },
-            )
+layout = html.Div(id='overall_eff',children=[
+    html.Div(
+        id='hello',
+        title='Hover to see me',
+        children=[
+        html.H1(
+            id="dash_header",
+            children=f'Overall Team wise Efficiency Percentage', 
+            style={"font-size":"25px", "font-align":"center"},
+        )], 
+            
+            ),
+    html.Br(),
+    dcc.Link('Back', href='/dash'),
+    html.Br(),
+    html.H2(children=f'Teams Efficiency bandwidth- Fiscal Year {min_year} - {max_year} (Till, {till_date})'),
+    dcc.Graph(
+        figure=fig_q1,
+        id="overall_efficiency"
+    ),
+    dash_table.DataTable(
+        data=df.to_dict('records'), 
+        # columns=[{"name": i, "id": i} for i in df.columns],
+        columns=[
+            {'name': ['Teams Ratings & Trend','Team'], 'id': 'team', 'type': 'text'},
+            {'name': ['Teams Ratings & Trend','Capacity'], 'id': 'capacity', 'type': 'numeric', 'format':dash_table.FormatTemplate.percentage(0)},
+            {'name': ['Teams Ratings & Trend','Ratings'], 'id': 'ratings', 'type': 'text'},
+            {'name': ['Teams Ratings & Trend','Trends'], 'id': 'trends', 'type': 'text'},
+        ],
+        merge_duplicate_headers=True,
+        style_cell={'textAlign': 'left', 'fontSize':'20px'},
+        style_header={
+            'backgroundColor': 'orange',
+            'fontWeight': 'bold',
+            'textAlign':'center',
+        },
+        )
 
-    ])
+])
 
 @dash_app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
@@ -142,10 +160,10 @@ def display_page(pathname):
     )
 def dash_into():
     # session['dash'].header = f"Welcome, {session['user'].name}!!!"
-    return f"Welcome, {g.user_role_id}!!!"
+    return f"Welcome, {g.user}!!!"
 
 
 @reports.route("/dash")
 # @login_required
-def dash_app():
+def dash_entry():
     return dash_app.index()
