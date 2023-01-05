93c79865386f375d5e5c670dd7ef83b9c3fc37af
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2023-01-05 22:54:08 +0530

    Update to show filter buttons in the dash app

--- hubble_reports/views/dash.py
+++ hubble_reports/views/dash.py
@@ -2,7 +2,7 @@ import dash
 import logging
 import pathlib
 
-from dash import Dash, dcc, html, callback
+from dash import Dash, dcc, html, callback, ctx
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
 from datetime import date
@@ -43,14 +43,28 @@ dash_app.index_string = html_body
 dash_app.layout = html.Div(
     [
         dcc.Location(id="url", refresh=False),
-        html.H1(id="topic-name",children="First"),
+        html.H1(id="topic-name", children="First"),
         html.Div(
             [
+                html.Div('Enter in "MM/DD/YYYY" format'),
                 dcc.DatePickerRange(
                     id="date-range-picker",
                     style={"width": 330},
-                    max_date_allowed=date.today()
+                    max_date_allowed=date.today(),
                 ),
+            ]
+        ),
+        html.Table(
+            id='button_postions',
+            children=[
+                html.Tr(
+                    [
+                    html.Td(html.Button("1 month", id="one_month_button", n_clicks=0)),
+                    html.Td(html.Button("6 months", id="six_month_button", n_clicks=0)),
+                    html.Td(html.Button("1 year", id="one_year_button", n_clicks=0)),
+                    ]
+                )
+                # html.Div(id="container-button-timestamp"),
             ]
         ),
         html.Div(id="page-content"),
@@ -67,16 +81,37 @@ dash_app.layout = html.Div(
     Output("max_date_range", "data"),
     Input("date-range-picker", "start_date"),
     Input("date-range-picker", "end_date"),
-    )
-def update_date_range(st_date, end_date):
+    Input('one_month_button', 'n_clicks'),
+    Input('six_month_button', 'n_clicks'),
+    Input('one_year_button', 'n_clicks'),
+)
+def update_date_range(st_date, end_date, btn1, btn2, btn3):
 
     if (not st_date) and (not end_date):
         st_date = date.today()
-        end_date = st_date - relativedelta.relativedelta(months=+6, days=+st_date.day-1)
-        # raise PreventUpdate
-    
-    logger.info(f"\n\n\n\n\nUPdate Date range\nStart Date:\n{st_date}\t{type(st_date)}\n\nEnd Date:\n{end_date}\t{type(end_date)}\n\n")
-    
+        end_date = st_date - relativedelta.relativedelta(
+            months=+6, days=+st_date.day - 1
+        )
+    logger.debug(f'\n\n\nButton clicked:\n{btn1 = }\t{btn2 = }\t{btn3 = }\n\n\n')
+    if "one_month_button" == ctx.triggered_id:
+        st_date = date.today()
+        end_date = st_date - relativedelta.relativedelta(
+            months=+1, days=+st_date.day - 1
+        )
+    elif "six_month_button" == ctx.triggered_id:
+        st_date = date.today()
+        end_date = st_date - relativedelta.relativedelta(
+            months=+6, days=+st_date.day - 1
+        )
+    elif "one_year_button" == ctx.triggered_id:
+        st_date = date.today()
+        end_date = st_date - relativedelta.relativedelta(
+            years=+1, days=+st_date.day - 1
+        )
+
+    logger.info(
+        f"\n\n\n\n\nUPdate Date range\nStart Date:\n{st_date}\t{type(st_date)}\n\nEnd Date:\n{end_date}\t{type(end_date)}\n\n"
+    )
     return st_date, end_date
 
 @reports.route("/report")
--- hubble_reports/views/pages/overall_efficiency_report.py
+++ hubble_reports/views/pages/overall_efficiency_report.py
@@ -26,73 +26,6 @@ dash.register_page(
 )
 
 db_conn = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
-df = pd.read_sql_query(
-    db.session.query(
-        db.func.avg(
-            100
-            * (
-                TimesheetEntry.authorized_hours
-                / ExpectedUserEfficiency.expected_efficiency
-            )
-        ).label("capacity"),
-        Team.name.label("team"),
-    )
-    .join(
-        ExpectedUserEfficiency, TimesheetEntry.user_id == ExpectedUserEfficiency.user_id
-    )
-    .join(Team, TimesheetEntry.team_id == Team.id)
-    .group_by(Team.name)
-    .statement,
-    db_conn,
-)
-
-
-# pd.read_sql_query(db.session.query(db.func.avg(100 * (TimesheetEntry.authorized_hours/ExpectedUserEfficiency.expected_efficiency)).label("capacity"), Team.name.label("team")).join(ExpectedUserEfficiency, TimesheetEntry.user_id == ExpectedUserEfficiency.user_id).join(Team, TimesheetEntry.team_id == Team.id).group_by(Team.name).statement, db_conn,)
-
-
-df_date = pd.read_sql_query(
-    db.session.query(
-        db.func.min(TimesheetEntry.entry_date).label("min_date"),
-        db.func.max(TimesheetEntry.entry_date).label("max_date"),
-    )
-    .join(
-        ExpectedUserEfficiency, TimesheetEntry.user_id == ExpectedUserEfficiency.user_id
-    )
-    .statement,
-    parse_dates=["min_date", "max_date"],
-    con=db_conn,
-)
-
-min_year = df_date["min_date"].dt.year[0]
-max_year = df_date["max_date"].dt.year[0]
-till_date = df_date["max_date"].dt.strftime("%B %Y")[0]
-
-df["ratings"] = df["capacity"].apply(
-    lambda a: "<h1>Excellent<\h1>"
-    if a > 121
-    else "Good"
-    if a > 120
-    else "Needs Improvement"
-)
-df["trends"] = df["capacity"].apply(
-    lambda a: "↑" if a > 121 else "↔︎" if a > 120 else "↓"
-)
-
-fig_bar = (
-    px.bar(
-        df,
-        x="team",
-        y="capacity",
-        color="team",
-        text_auto=True,
-        text="capacity",
-        title="Teams Capacity - Efficiency %",
-        labels={"team": "Teams", "capacity": "Capacity"},
-    )
-    .update_traces(texttemplate="%{y:0.0f}%")
-    .update_layout(title_x=0.5)
-)
-
 layout = html.Div(
     id="overall_eff",
     children=[
@@ -113,7 +46,6 @@ layout = html.Div(
         html.Br(),
         html.H2(
             id="overall_efficiency_title",
-            # children=f"Teams Efficiency bandwidth- Fiscal Year {min_year} - {max_year} (Till, {till_date})",
         ),
         dcc.Link(
             dcc.Graph(
@@ -247,10 +179,6 @@ def update_figure(st_date, end_date):
         con=db_conn,
     )
 
-    # min_year = st_date
-    # max_year = end_date
-    till_date = for_str_date_to_new_str_date(end_date, r"%Y-%m-%d", r"%B %Y") #datetime.strftime(datetime.strptime(end_date, r"%Y-%m-%d"), r"%B %Y")#end_date.strftime("%B %Y")
-    logger.debug(f"\n\n\nTill date:\t{till_date = }\n\n")
     df["ratings"] = df["capacity"].apply(
         lambda a: "<h1>Excellent<\h1>"
         if a > 121
@@ -297,17 +225,10 @@ def update_figure(st_date, end_date):
 
 @callback(
     Output("team_selected", "data"),
-    # Output("min_date_range", "data"),
-    # Output("max_date_range", "data"),
     Input("overall_efficiency", "clickData"),
 )
 def store_data(clickdata):
     if not clickdata:
         raise PreventUpdate
     column = clickdata["points"][0]["x"]
-    max_date = date.today()
-    min_date = max_date - relativedelta.relativedelta(months=+6, days=+max_date.day - 1)
-    logger.debug(
-        f'\n\n\nDefault dates:\nCurrent date:\n{str(max_date.strftime(r"%Y-%m-%d"))}\nLast 6 months:\n{str(min_date.strftime(r"%Y-%m-%d"))}'
-    )
-    return column  # , str(min_date.strftime(r"%Y-%m-%d")), str(max_date.strftime(r"%Y-%m-%d"))
+    return column 
