18d100e8162c9705f7bb3b537b533fda32945257
Sudhakar Sivamani <sudhakar@mallow-tech.com>
2023-01-09 08:19:57 +0530

    comment for clarification

--- hubble_reports/views/pages/not_found_404.py
+++ hubble_reports/views/pages/not_found_404.py
@@ -3,4 +3,6 @@ import dash
 
 dash.register_page(__name__)
 
+#This is used to remove the page not found in index page,
+#In future this will be removed, as report will be set as front page as index
 layout = html.H1("")
