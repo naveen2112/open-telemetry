3b4b89608e85f66ca5d3f1b03ed2266136ba9d54
sudhakar_s8 <sudhakar@mallow-tech.com>
2023-01-12 09:59:49 +0530

    Remove unused file

--- hubble_reports/views/pages/not_found_404.py
+++
@@ -1,8 +0,0 @@
-from dash import html
-import dash
-
-dash.register_page(__name__)
-
-#This is used to remove the page not found in index page,
-#In future this will be removed, as report will be set as front page as index
-layout = html.H1("")
