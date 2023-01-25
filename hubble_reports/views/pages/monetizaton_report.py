cc57bfa990b120ee565dee2a8fddd81e81d2c4da
sudhakar_s8 <sudhakar@mallow-tech.com>
2023-01-25 17:59:40 +0530

    Update description and figure to use less for loop

--- hubble_reports/views/pages/monetizaton_report.py
+++ hubble_reports/views/pages/monetizaton_report.py
@@ -28,6 +28,25 @@ db_connection = create_engine(current_app.config.get("SQLALCHEMY_DATABASE_URI"))
 
 
 layout = [
+    html.Div(
+        children=[
+        html.H2('Description:'),
+        html.Div(
+            id='description',
+            children=[
+                html.Li(
+            children=r'Red region shows the monetisation gap % exceeding 10%, While the green region shows monetisation gap % within limit'
+        ),
+        html.Li(
+            children=r'Monetisation gap % having negative % are shown as 0% in the graph'
+        ),
+        ],
+        style={
+            'paddingLeft':'25px',
+        }
+        )
+        ]
+    ),
     dcc.Loading(
         type="default",
         children=dcc.Graph(
@@ -36,9 +55,12 @@ layout = [
     ),)
 ]
 
-def create_line_chart(df):
+def create_line_chart(df, row, column, fig_subplots):
+    figure_traces = []
+    for i, team in enumerate(df['Teams'].unique()):
+        df_team = df[df['Teams']==team].copy()
         fig = px.line(
-            df, 
+            df_team, 
             x='Date', 
             y='Gap',
             text='Gap',
@@ -49,17 +71,19 @@ def create_line_chart(df):
             title='Teams',
         )
         fig.update_traces(
-            marker=dict(color=df['team_color']),
-            line=dict(color=df['team_color'].unique()[0]),
+            marker=dict(color=df_team['team_color']),
+            line=dict(color=df_team['team_color'].unique()[0]),
             texttemplate="%{y:0.01f}%",
         )
         
         
-        figure_traces = []
-        print(fig)
-        for trace in range(len(fig["data"])):
-            figure_traces.append(fig["data"][trace])
-        return figure_traces
+       
+        print(f'{i+1} Before traces:\n',fig)
+        # for trace in range(len(fig["data"])):
+        #     figure_traces.append(fig["data"][trace])
+        fig_subplots.append_trace(fig['data'][0], row= i//column +1, col=int(i%column)+1)
+        print(f'\n{i+1} After traces:\n',fig_subplots)
+    return fig_subplots
 
 @callback(
     Output('monetization-report', 'figure'),
@@ -107,76 +131,20 @@ def monetization_report(urlname, min_date_sess, max_date_sess):
     df["team_color"] = "white"
     df['Date'] = df["Date"].dt.strftime(r'%b %y')
     
+    
     for i, team in enumerate(df.Teams.unique()):
         df["team_color"].loc[df["Teams"] == team] = px.colors.qualitative.Dark24[i]
-    figure = px.line(
-        df,
-        x="Date",
-        y="Gap",
-        color="Teams",
-        text="Gap",
-        hover_data={
-            "Gap": ":.2f",
-            # "Teams": False,
-            "Date": False,
-            "text": False,
-            "color": False,
-        },
-        markers="circle",
-        # hover_name='Teams',
-        facet_col="Teams",
-        facet_row_spacing=0.025,
-        facet_col_wrap=2,
-        # facet_col_spacing=0.01,
-        labels={
-            "Teams": "Teams",
-        },
-    )
-    figure.update_traces(
-        texttemplate="<b>%{y:0.01f}%</b>",)
-    figure.update_yaxes(
-        # range=[0, df["Gap"].max() + 7],
-    )
-    figure.update_layout(
-        template="plotly_white",
-        hovermode="x unified",
-        height=700,
-    )
-    figure.update_traces(
-        textposition="top center",
-    )
-    figure.update_annotations(
-        textangle=0,
-        xanchor="left",
-        xref="paper",
-        yanchor="top",
-        y=df["Gap"].max() + 7,
-        yref="y domain",
-        # y='relative',
-        x=0.5,
-        # y=17,
-        # y=1,
-    )
-    for da in figure["data"]:
-        if da["type"] in ["scatter", "line"]:
-            da["line"]["color"] = df[df["Teams"] == da["name"]]["team_color"].unique()[0]
-        elif da["type"] == "bar":
-            da["marker"]["color"] = df[df["Teams"] == da["name"]]["team_color"].unique()[0]
-    for i, da in enumerate(figure["layout"]["annotations"]):
-        da["text"] = da["text"].replace("Teams=", "")
-        da["yref"] = "y" + str(i + 1 if i > 0 else "")
-        da["x"] = 0.25 if i%2 == 0 else 0.75
-    
-    # return figure
 
     no_of_teams = len(df['Teams'].unique())
+    row = int(ceiling(no_of_teams/2,1))
+    column = 2
     fig_subplots = make_subplots(
-    rows=int(ceiling(no_of_teams/2,1)),#len(df['Teams'].unique()), 
-    cols=2, 
+    rows=row,#len(df['Teams'].unique()), 
+    cols=column, 
     # shared_xaxes='columns',
     # shared_yaxes=True,
-    vertical_spacing=0.15,
-    horizontal_spacing=0.05,
+    vertical_spacing=0.1,
+    horizontal_spacing=0.03,
     subplot_titles=df['Teams'].unique(),
     start_cell='bottom-left',
     y_title='Gap, %',
@@ -192,34 +160,8 @@ def monetization_report(urlname, min_date_sess, max_date_sess):
     max_gap = ceiling(df['Gap'].max()*1.25, 5)
     no_of_dates = len(df['Date'].unique())
     
-
-    for i, team in enumerate(df.Teams.unique()):
-        unique_team_df = df[df['Teams']==team].copy()
-        figure_traces = create_line_chart(unique_team_df)
-        for traces in figure_traces:
-            fig_subplots.append_trace(traces, row= i//2 +1, col=int(i%2)+1)
-
-        data=fig_subplots['data'][i]
-        x_axis = data['xaxis']
-        y_axis = data['yaxis']
-        # fig_subplots.add_layout_image(
-        #     dict(
-        #         source=f"static/images/png/{team.lower()}.png",
-        #         xref=x_axis,
-        #         yref=y_axis,
-        #         xanchor='center',
-        #         yanchor='middle',
-        #         # x=len(df[df["Teams"] == team]['Date'].unique())/2-0.5,
-        #         x=no_of_dates/2-0.5,
-        #         y=max_gap/2,
-        #         sizex=1 if no_of_dates >1 else 0.5,
-        #         sizey=max_gap/2,
-        #         sizing="stretch",
-        #         opacity=0.5,
-        #         layer="below",
-        #     ),
-        # )
-
+    fig_subplots = create_line_chart(df, row, column, fig_subplots)
+    fig_subplots.update_yaxes(showgrid=False)
     fig_subplots.add_hrect(
         y0=-10,
         y1=10,
@@ -241,7 +183,6 @@ def monetization_report(urlname, min_date_sess, max_date_sess):
 
     fig_subplots.update_yaxes(
         range=[-10, max_gap],
-        # title='Gap, %',
     )
 
     fig_subplots.update_traces(textposition="top center")
@@ -249,7 +190,8 @@ def monetization_report(urlname, min_date_sess, max_date_sess):
     fig_subplots.update_layout(
         hovermode='x',
         template="plotly_white",
+        margin=dict(t=50, r=12, l=65)
         )
-    print(fig_subplots)
+    # print(fig_subplots)
 
     return fig_subplots
