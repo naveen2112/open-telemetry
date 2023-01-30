baa4e04d1fc16bfceedee273218b4bdb47cca087
sudhakar_s8 <sudhakar@mallow-tech.com>
2023-01-30 06:40:12 +0530

    Update to use "raise" instead of "return" for PreventUpdate

--- hubble_reports/views/pages/efficiency_report.py
+++ hubble_reports/views/pages/efficiency_report.py
@@ -162,7 +162,7 @@ def overall_efficiency_report(st_date, end_date):
 def detailed_efficiency_report(team, min_date_sess, max_date_sess):
 
     if not team:
-        return PreventUpdate
+        raise PreventUpdate
 
     df = pd.read_sql_query(
         db.session.query(
