035d04363bcf4d5b7a32ff7e2d0a7b2db91e57fc
Tariqjamal Abdulazeez <tariqjamal@mallow-tech.com>
2023-04-04 17:20:50 +0530

    correct the alignment

--- hubble/middlewares/subdomain_classifier.py
+++ hubble/middlewares/subdomain_classifier.py
@@ -2,7 +2,9 @@ class SubdomainClassifier:
     def __init__(self, get_response):
         self.get_response = get_response
 
+
     def __call__(self, request):
-        request.subdomain = request.get_host().split('.')[0]
-        request.urlconf = request.subdomain+'.urls' 
+        request.subdomain = request.get_host().split(".")[0]
+        request.urlconf = request.subdomain + ".urls"
         return self.get_response(request)
+
