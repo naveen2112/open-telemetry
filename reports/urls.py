aa2b8ad923e5bebc678c14cca9ef936705e7c9d2
Palaniyappan Ramanathan <palaniyappan@mallow-tech.com>
2023-04-13 15:58:12 +0530

    Update django orm

--- reports/urls.py
+++ reports/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from reports import views
 
 urlpatterns = [
+    path("auth", include("hubble.urls")),
     path("", views.index, name="index"),
-    path("datatable", views.datatable.as_view(), name="datatable"),
     path("monetization", views.monetization_report, name="monetization"),
     path("kpi", views.kpi_report, name="kpi"),
+    path("datatable", views.datatable.as_view(), name="datatable"),
     path("monetization_datatable", views.monetization_datatable.as_view(), name="monetization_datatable"),
     path("kpi_datatable", views.kpi_datatable.as_view(), name="kpi_datatable"),
-    path("auth", include("hubble.urls")),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from hubble.models import TimesheetEntry, ProjectResource
+from hubble.models import TimesheetEntry
 from datetime import datetime
 from django.db.models.functions import Coalesce
-from django.db.models import Avg, F, Q, Sum, Case, When, FloatField, Subquery, OuterRef
+from django.db.models import (
+    Avg,
+    F,
+    Q,
+    Sum,
+    Case,
+    When,
+    FloatField,
+    Func,
+    Value,
+    CharField,
+)
 from core.utils import CustomDatatable
-from django.db.models.functions import TruncMonth
 
 now = datetime.now()
 
@@ -113,29 +123,47 @@ class datatable(CustomDatatable):
                 )
                 & Q(entry_date__range=("2022-01-01", "2023-01-01"))
             )
-            .values("team__name")
+            .annotate(
+                Month=Func(
+                    F("entry_date"),
+                    Value("MonthYYYY"),
+                    function="to_char",
+                    output_field=CharField(),
+                ),
+            )
+            .values("Month")
             .filter(team__id=pk)
             .annotate(
-                capacity=Avg(
+                Team_Name=F("team__name"),
+                Expected_hours=Sum(
+                    "user__expected_user_efficiencies__expected_efficiency"
+                ),
+                Actual_hours=Sum("authorized_hours"),
+                Capacity=Avg(
                     100
                     * (F("authorized_hours"))
                     / (F("user__expected_user_efficiencies__expected_efficiency"))
                 ),
-                actual_hours=Sum("authorized_hours"),
-                expected_hours=Sum(
-                    "user__expected_user_efficiencies__expected_efficiency"
-                ),
             )
         )
-        html = '<table class="row-details">'
-        for tag, value in list(data)[0].items():
-            html += "<tr><td>%s</td><td>%s</td></tr>" % (tag, value)
-        html += "</table>"
+        html = '<table class="display border-0 table-with-no-border dataTable no-footer"><tr><thead>'
+        for header in list(data)[0].keys():
+            html += "<td>%s</td>" % (header)
+        html += "</thead><tbody>"
+        for field in list(data):
+            html += "<tr>"
+            for value in field.values():
+                html += "<td>%s</td>" % (value)
+            html += "</tr>"
+        html += "</tbody></table>"
         return html
 
 
 class monetization_datatable(CustomDatatable):
     model = TimesheetEntry
+    initial_order = [
+        ["team__name", "asc"],
+    ]
     show_column_filters = False
     column_defs = [
         {
@@ -173,21 +201,35 @@ class monetization_datatable(CustomDatatable):
                 )
                 & Q(entry_date__range=("2022-01-01", "2023-01-01"))
             )
-            .values("team__name")
+            .annotate(a_sum=Sum("authorized_hours"))
             .annotate(
-                day=TruncMonth("entry_date"),
+                day=Func(
+                    F("entry_date"),
+                    Value("MonthYYYY"),
+                    function="to_char",
+                    output_field=CharField(),
+                ),
+            )
+            .values("day", "team__name")
+            .annotate(
                 gap=Case(
-                    When(authorized_hours__lte=0, then=0),
+                    When(a_sum=0, then=0),
                     default=(
                         100
                         * (
-                            Sum("user__expected_user_efficiencies__expected_efficiency")
-                            - Sum("authorized_hours")
+                            (
+                                Sum(
+                                    F(
+                                        "user__expected_user_efficiencies__expected_efficiency"
+                                    )
+                                )
+                                - Sum(F("authorized_hours"))
+                            )
+                            / Sum("authorized_hours")
                         )
-                    )
-                    / Sum("authorized_hours"),
+                    ),
                     output_field=FloatField(),
-                ),
+                )
             )
         )
         return data
@@ -238,27 +280,20 @@ class kpi_datatable(CustomDatatable):
             "title": "Expected_Efficiency",
             "visible": True,
             "searchable": True,
-        }
+        },
     ]
 
+
     def get_initial_queryset(self, request=None):
-        subquery1 = (
-            ProjectResource.objects.select_related("user")
-            .filter(user__id=OuterRef("user"))
-            .values("user__team__name")[:1]
-        )
-        subquery2 = (
-            ProjectResource.objects.select_related("user")
-            .filter(user__id=OuterRef("user"))
-            .values("user__name")[:1]
-        )
         data = (
             TimesheetEntry.objects.select_related("project", "user")
             .filter(
                 Q(
                     entry_date__range=(
                         F("user__expected_user_efficiencies__effective_from"),
-                        Coalesce(F("user__expected_user_efficiencies__effective_to"), now),
+                        Coalesce(
+                            F("user__expected_user_efficiencies__effective_to"), now
+                        ),
                     )
                 )
             )
@@ -267,11 +302,13 @@ class kpi_datatable(CustomDatatable):
             .annotate(
                 Project=F("project__name"),
                 Working_hours=Sum("working_hours"),
-                expected_efficiency=F("user__expected_user_efficiencies__expected_efficiency"),
+                expected_efficiency=F(
+                    "user__expected_user_efficiencies__expected_efficiency"
+                ),
                 Authorized_sum=Sum("authorized_hours"),
                 Billed_sum=Sum("billed_hours"),
-                User_name=Subquery(subquery2),
-                Team_name=Subquery(subquery1),
+                User_name=F("user__name"),
+                Team_name=F("team__name"),
             )
             .values(
                 "Project",
