cc3a44cf31c8400d7cae84b0231f8ed9b00725e7
Palaniyappan Ramanathan <palaniyappan@mallow-tech.com>
2023-05-16 12:29:04 +0530

    Address PR comments

--- hubble/views.py
+++ hubble/views.py
@@ -10,17 +10,16 @@ from hubble.models import User
 from core import constants
 
 
+#To make sure correct authentication method is renderred
 def login(request):
     context = {}
-    if ENV_NAME == constants.ENVIRONMENT_DEVELOPMENT:
-        context["login_method"] = constants.ENVIRONMENT_DEVELOPMENT
-    else:
-        context["login_method"] = constants.ENVIRONMENT_PRODUCTION
+    context["login_method"] = ENV_NAME #This context variable will be used in template to render correct authentication method 
     return render(request, "auth/login.html", context)
 
 
+#Authenticates the user
 def signin(request):
-    if ENV_NAME == constants.ENVIRONMENT_DEVELOPMENT:
+    if ENV_NAME == constants.ENVIRONMENT_DEVELOPMENT: #To ensure the authentication method
         if request.method == "POST":
             user_email = request.POST.get("email")
             if User.objects.filter(email=user_email).exists():
@@ -47,18 +46,20 @@ def signin(request):
         return HttpResponseRedirect(flow["auth_uri"])
 
 
+#Logout feature
 def signout(request):
     auth_helper.remove_user_and_token(request)
     logout(request)
     return HttpResponseRedirect(reverse("login"))
 
 
+#Redirects to Microsoft SSO
 def callback(request):
     result = auth_helper.get_token_from_code(request)
     data = result["id_token_claims"]
     user_crendentials = data["preferred_username"]
     user = User.objects.get(email=user_crendentials)
-    if user is not None:
+    if user is not None: #Checks whether the authenticated member is form Mallow or no, by checking with Database
         login(request, user)
         return redirect("login")
     else:
