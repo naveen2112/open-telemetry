5a784854cb310047543cf79766983bf1956ec991
Palaniyappan Ramanathan <palaniyappan@mallow-tech.com>
2023-05-16 13:12:33 +0530

    Address PR comments

--- hubble/middlewares/subdomain_classifier.py
+++ hubble/middlewares/subdomain_classifier.py
@@ -4,9 +4,7 @@ class SubdomainClassifier:
 
 
     def __call__(self, request):
-        request.subdomain = (request.get_host().split(".")[0]).split("-")[
-            0
-        ]  # Get the sub-domain from request
+        request.subdomain = (request.get_host().split(".")[0]).split("-")[0]  # Get the sub-domain from request
         if request.subdomain == "training" or request.subdomain == "reports":
             request.urlconf = request.subdomain + ".urls"
         return self.get_response(request)
