2e48de2d924c349929d2a8e950af1afcd822d13d
Poonkawin Saravanan <poonkawin@mallow-tech.com>
2023-08-08 18:33:23 +0530

    create new table trainee holidays

---
+++ hubble/models/trainee_holiday.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from core import db
+from hubble.models import User, Batch
+
+
+class TraineeHoliday(db.SoftDeleteWithBaseModel):
+    id = models.BigAutoField(primary_key=True)
+    batch = models.ForeignKey(Batch, on_delete=models.CASCADE)
+    date_of_holiday = models.DateField()
+    month_year = models.CharField(max_length=255)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    reason = models.CharField(max_length=255, blank=True, null=True)
+    allow_check_in = models.BooleanField()
+
+    class Meta:
+        managed = True
+        db_table = "trainee_holidays"
+
+    def __str__(self):
+        return str(self.date_of_holiday)
